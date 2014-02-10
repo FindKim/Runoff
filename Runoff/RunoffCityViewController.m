dc5a68f62fc6f4126110311bfcd478f890fb2b69
Owen Zidar <owenzidar@owens-mbp.dhcp.nd.edu>
2014-02-09 23:05:50 -0500

    Comments. To figure out function for image scaling

--- Runoff/RunoffCityViewController.m
+++ Runoff/RunoffCityViewController.m
@@ -15,7 +15,33 @@
 
 @implementation RunoffCityViewController
 
+/*
 //-(void)
+
+- (int)____:(NSArray *)___
+{
+
+    
+return ____
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *aTouch in touches) {
+        if (aTouch.tapCount >= 2) {
+            // The view responds to the tap
+            [self respondToDoubleTapGesture:aTouch];
+        }
+    }
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+}
 
 //Scale factor S
 //Iy = inset y
@@ -25,4 +51,5 @@
 //column = (8/1200)(Iy + S*y)
 //row = (8/1200)(Ix + S*x)
 
+*/
 @end
