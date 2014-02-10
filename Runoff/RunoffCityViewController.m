6c0c45077c3e9eb2027dda52733e97b7f5f83618
Owen Zidar <owenzidar@owens-mbp.dhcp.nd.edu>
2014-02-09 23:10:35 -0500

    This is useless

--- Runoff/RunoffCityViewController.m
+++ Runoff/RunoffCityViewController.m
@@ -15,41 +15,4 @@
 
 @implementation RunoffCityViewController
 
-/*
-//-(void)
-
-- (int)____:(NSArray *)___
-{
-
-    
-return ____
-}
-
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-}
-
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-}
-
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    for (UITouch *aTouch in touches) {
-        if (aTouch.tapCount >= 2) {
-            // The view responds to the tap
-            [self respondToDoubleTapGesture:aTouch];
-        }
-    }
-}
-
-- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
-}
-
-//Scale factor S
-//Iy = inset y
-//Ix = inset x
-//1200 points down and across
-//8 units down and across
-//column = (8/1200)(Iy + S*y)
-//row = (8/1200)(Ix + S*x)
-
-*/
 @end
