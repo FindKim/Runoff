1ecfea6e6257298b9638393d5d4cb1a1f8df29c1
Kim Ngo <kimngo@nds-macbook-pro.dhcp.nd.edu>
2014-02-13 20:59:22 -0500

    First attempt at deleting placed biofilters biofilters are being deleted after every double tap

--- Runoff/RunoffCityViewController.m
+++ Runoff/RunoffCityViewController.m
@@ -13,6 +13,11 @@
 @property (nonatomic, strong) UIImage *cityImage; //_cityImage instance variable
 @property (nonatomic, strong) UIImageView *cityImageView;
 @property (nonatomic, strong) UIView *container;
+@property (weak, nonatomic) IBOutlet UILabel *countLabel;
+@property (nonatomic) int biofilterCount;
+//@property (nonatomic, strong) NSArray *biofilterArray;
+//@property (nonatomic, strong) UIImage *biofilterImage;
+
 
 @end
 
@@ -64,24 +69,89 @@
 - (void)scrollViewDidEndZooming:(UIScrollView *)sender withView:(UIView *)zoomView atScale:(CGFloat)scale {
 }
 
+- (void)setBiofilterImage:(UIImage *)biofilterImage {
+    self.biofilterImage = [UIImage imageNamed:@"Biofilter"];
+}
+
+//- (void)setBiofilterArray:(NSArray *)biofilterArray {
+//    self.biofilterArray = [NSArray arrayWithObjects:
+//                            [UIImage imageNamed:@"Biofilter"],
+//                            [UIImage imageNamed:@"Biofilter"],
+//                            [UIImage imageNamed:@"Biofilter"],
+//                            nil];
+//}
+
+
 - (void)placeBiofilterAtPoint:(CGPoint)mypoint{
     UIImage * biofilterImage = [UIImage imageNamed:@"Biofilter"];
     UIImageView * biofilterView = [[UIImageView alloc] initWithImage:biofilterImage];
     [self.container addSubview:biofilterView];
     biofilterView.frame = CGRectMake(mypoint.x, mypoint.y, 20, 20);
     biofilterView.center = mypoint;
+    // trying to make image recognize touch gestures to double tap to remove image
+//    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTapGestureCaptured:)];
+//    [biofilterView addGestureRecognizer:doubleTap];
+//    [biofilterView setMultipleTouchEnabled:YES];
+//    [biofilterView setUserInteractionEnabled:YES];
+}
+/*
+- (void)doubleTapGestureCaptured:(UITapGestureRecognizer *)gesture {
+    self.biofilterView = [gesture.view hitTest:gesture locationInView:gesture.view withEvent:nil];
+}
+*/
+- (void)setBiofilterCount:(int)biofilterCount {
+    _biofilterCount = biofilterCount;
+    self.countLabel.text = [NSString stringWithFormat:@"count: %d", self.biofilterCount];
 }
 
 
+- (BOOL) point:(CGPoint) pt isonRect:(CGRect)rpt {
+    if (pt.x >= rpt.origin.x && pt.x <= rpt.size.width + rpt.origin.x) {
+        // within x bounds
+        if (pt.y >= rpt.origin.y && pt.y <= rpt.size.height + rpt.origin.y) {
+            // within y bounds
+            return YES;
+        }
+    }
+    return NO;
+}
+
+
+// Needs to delete biofilter within range of nearest biofilter
+
 - (IBAction)mapDoubleTap:(UITapGestureRecognizer *)sender {
+
     CGPoint mypoint = [sender locationInView:self.container];
     //determines point at which the user double taps the screen
+
     NSLog(@"x = %f,y = %f", mypoint.x, mypoint.y);
+    
     float col = (8.0/320.0)*(mypoint.y);
     float row = (8.0/320.0)*(mypoint.x);
+    
     NSLog(@"col = %f, row = %f", col, row);
-    if(YES){
+    
+    if(self.biofilterCount<5){
+        
+        for (UIView *view in self.container.subviews) {
+            
+            if(view != self.cityImageView) {
+                
+//                CGPoint pt = [mypoint locationInView:self.container];
+//                view.frame;
+                if([self point:mypoint isonRect:self.container.bounds]) {
+                    
+                    [view removeFromSuperview];
+                    
+                }
+                
+            }
+            
+        }
+    
         [self placeBiofilterAtPoint:mypoint];
+        self.biofilterCount++;
+        NSLog(@"count = %d", self.biofilterCount);
     }
     //Scale factor S
     //Iy = inset y
@@ -91,5 +161,7 @@
     //column = (8/1200)(Iy + S*y)
     //row = (8/1200)(Ix + S*x)
 }
+
+
 
 @end
